78ca58c09c4e1d6ed22fbeebfc6f7138994bc803
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-10-25 18:36:32 +0200

    set migration versions to 7.1

--- db/migrate/20241021183034_create_good_jobs.rb
+++ db/migrate/20241021183034_create_good_jobs.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateGoodJobs < ActiveRecord::Migration[7.2]
+class CreateGoodJobs < ActiveRecord::Migration[7.1]
   def change
     # Uncomment for Postgres v12 or earlier to enable gen_random_uuid() support
     # enable_extension 'pgcrypto'
